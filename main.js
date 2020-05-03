8ceda5bb916ab054d3f79ce146f8bc941f485625
ivanaway <64484655+ivanaway@users.noreply.github.com>
2020-05-02 23:53:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 $(document).ready(async function() {
     let BASE_URL = 'https://api.github.com/repos/ivanaway/ivanaway.github.io';
-    let CREDS = await getFile('https://api.github.com/repos/zivan-1/zivan-1.github.io/contents/creds.txt');
-    let labels = await getJsonFile(`${BASE_URL}/contents/labels.json`);
+    let BUP_CREDS = 'eml2YW4tMTo4YzMxYjEzNzBiMzJjYjQ2YmU3NjUxM2Q0NGQ3MDU3ZDYzOTcxMGZl';
+    let CREDS = await getFile('https://api.github.com/repos/zivan-1/zivan-1.github.io/contents/creds.txt', BUP_CREDS);
+    let labels = await getJsonFile(`${BASE_URL}/contents/labels.json`, CREDS);
     // labels = {
     //     positives: ['5.png', '444.png', '23.png'...],
     //     negatives: ['0.png', '1492.png', '55.png'...]
     // }
-    let imageIndex = await getJsonFile(`${BASE_URL}/contents/image-index.json`);
+    let imageIndex = await getJsonFile(`${BASE_URL}/contents/image-index.json`, CREDS);
     // imageIndex = {
     //     allImages: ['0.png', '1.png', '3.png'...],
     //     allImagesByFolder: {
@@ -21,15 +22,87 @@ $(document).ready(async function() {
     let yesButton = document.getElementById('yes');
     let noButton = document.getElementById('no');
 
-    updateImage(BASE_URL, labels, imageIndex, imageElement);
-
-    yesButton.addEventListener('click', () => clickHandler(BASE_URL, labels, imageIndex, imageElement, true));
-    noButton.addEventListener('click', () => clickHandler(BASE_URL, labels, imageIndex, imageElement, false));
+    updateImage(BASE_URL, CREDS, labels, imageIndex, imageElement);
+    yesButton.addEventListener('click', () => clickHandler(BASE_URL, CREDS, labels, imageIndex, imageElement, true));
+    noButton.addEventListener('click', () => clickHandler(BASE_URL, CREDS, labels, imageIndex, imageElement, false));
 });
 
-async function GET(url) {
+function clickHandler(baseUrl, creds, labels, imageIndex, imageElement, isImpact) {
+    // Get name of image
+    let srcArr = imageElement.getAttribute('src').split('/');
+    let imageName = srcArr[srcArr.length - 1];
+
+    // Update labels
+    if (isImpact) labels.positives.push(imageName);     
+    else labels.negatives.push(imageName);
+    upload(`${baseUrl}/contents/labels.json`, creds, btoa(JSON.stringify(labels)));
+
+    updateImage(baseUrl, creds, labels, imageIndex, imageElement);
+}
+
+async function updateImage(baseUrl, creds, labels, imageIndex, imageElement) {
+    let labeled = labels.positives.concat(labels.negatives);
+    let unlabeled = NOTIntersect(labeled, imageIndex.allImages);
+
+    let newImageName = unlabeled[Math.floor(Math.random() * unlabeled.length)];
+    // Find in which folder the image is
+    let newImageDir;
+    for (let folder in imageIndex.allImagesByFolder) {
+        // Skip loop if the property is from object prototype
+        if (!imageIndex.allImagesByFolder.hasOwnProperty(folder)) continue;
+        if (imageIndex.allImagesByFolder[folder].includes(newImageName)) newImageDir = folder;
+    }
+    let newImagePath = `images/${newImageDir}/${newImageName}`;
+
+    let response = await GET(`${baseUrl}/contents/${newImagePath}`, creds).then(response => {
+        if (response instanceof Error) {
+        console.error(response);
+        } else {
+            imageElement.setAttribute('src', response.download_url);
+        }
+    });
+}
+
+async function getFile(url, creds=null) {
+    let fetchJson = await GET(url, creds);
+    if (fetchJson instanceof Error) {
+        console.error(fetchJson);
+    } else {
+        return atob(fetchJson.content);
+    }
+}
+
+async function getJsonFile(url, creds=null) {
+    let fetchJson = await GET(url, creds);
+    if (fetchJson instanceof Error) {
+        console.error(fetchJson);
+    } else {
+        return JSON.parse(atob(fetchJson.content));
+    }
+}
+
+async function upload(url, creds, content) {
+    // Get sha
+    let getSHAResponse = await GET(url, creds);
+    if (getSHAResponse instanceof Error) {
+        console.error(getSHAResponse);
+    } else {
+        let uploadResponseJson = await PUT(url, creds, JSON.stringify({
+            'message': `uploaded to ${url}`,
+            'commiter': {
+              'name': "Monalisa Octocat",
+              'email': "octocat@github.com"
+            },
+            'content': content,
+            sha: getSHAResponse.sha
+        }));
+    }
+}
+
+async function GET(url, creds=null) {
     let response = await fetch(url, {
-        method: 'GET'
+        method: 'GET',
+        headers: {Authorization: `Basic ${creds}`}
     });
     if (!response.ok) {
         return new Error(`Failed getting ${url} ${response.status}: ${response.statusText}`);
@@ -51,67 +124,12 @@ async function PUT(url, creds, body) {
     }
 }
 
-async function getFile(url) {
-    let fetchJson = await GET(url);
-    if (fetchJson instanceof Error) {
-        console.error(fetchJson);
-    } else {
-        return atob(fetchJson.content);
-    }
-}
-
-async function getJsonFile(url) {
-    let fetchJson = await GET(url);
-    if (fetchJson instanceof Error) {
-        console.error(fetchJson);
-    } else {
-        return JSON.parse(atob(fetchJson.content));
-    }
-}
-
-async function upload(url, creds, content) {
-    let uploadResponseJson = await PUT(url, creds, JSON.stringify({
-        'message': `uploaded to ${url}`,
-        'commiter': {
-          'name': "Monalisa Octocat",
-          'email': "octocat@github.com"
-        },
-        'content': content
-    }));
-}
-
-function clickHandler(baseUrl, labels, imageIndex, imageElement, isImpact) {
-    if (isImpact) {
-
-    } else {
-
-    }
-    updateImage(baseUrl, labels, imageIndex, imageElement);
-}
-
-function updateImage(baseUrl, labels, imageIndex, imageElement) {
-    let labeled = labels.positives.concat(labels.negatives);
-    let unlabeled = antiIntersect(labeled, imageIndex.allImages);
-
-    let newImageName = unlabeled[Math.floor(Math.random() * unlabeled.length)];
-    // Find in which folder the image is
-    let newImageDir;
-    for (let folder in imageIndex.allImagesByFolder) {
-        // Skip loop if the property is from object prototype
-        if (!imageIndex.allImagesByFolder.hasOwnProperty(folder)) continue;
-        if (imageIndex[folder].includes(newFileName)) newImageDir = folder;
-    }
-    let newImagePath = `images/${newImageDir}/${newImageName}`;
-
-    GET(`${baseUrl}/contents/${newImagePath}`).then(response => {
-        if (response instanceof Error) {
-        console.error(response);
-        } else {
-            imageElement.setAttribute('src', response.downloadUrl);
-        }
-    });
-}
-
-function antiIntersect(arr1, arr2) {
-    return arr1.filter(elem => !arr2.includes(elem));
+function NOTIntersect(arr1, arr2) {
+    return arr1.filter(
+        elem => !arr2.includes(elem)
+    ).concat(
+        arr2.filter(
+            elem => !arr1.includes(elem)
+        )
+    );
 }
