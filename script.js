d16e9128c8f59d5642295218f54da43d9bf6a68a
ivanz-tech <64484655+ivanz-tech@users.noreply.github.com>
2021-02-03 20:35:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let newsApiKey = 'feef1da8cb7732bae46deb391e9d0f83' // lol
 
-fetch('http://api.datanews.io/v1/news?q=SpaceX&from=2020-07-01&to=2020-09-10&language=en&apiKey=0aszfc72ctgxr4e947mg1sr67', {
+fetch('https://api.datanews.io/v1/news?q=SpaceX&from=2020-07-01&to=2020-09-10&language=en&apiKey=0aszfc72ctgxr4e947mg1sr67', {
     method: 'get',
     headers: {
         'Access-Control-Allow-Origin': '*'    
