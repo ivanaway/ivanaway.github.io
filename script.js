b99c9eb33820a5995692312582db019973548186
Ivan Zabrodin <pyrat32@gmail.com>
2021-02-06 13:01:38 -0600

    lazy commit message

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function stockSearchHandler(event) {
 
 fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=U65M3D2LOCIOUFEM', {
 }).then(response => response.json()).then(data =>console.log(data))
+
+function getCrypto() {
+    var requestUrl = 'https://api.coinbase.com/v2/exchange-rates';
+    console.log(requestUrl);
+    fetch(requestUrl)
+      .then(function (response) {
+        console.log(response)
+        if (response.ok) {
+          return response.json()
+            .then(function (cryptoData) {
+              if (cryptoData["Error Message"]) {
+                return $('#errorModal').foundation('open')
+              }
+              console.log("crypto data: ", cryptoData)
+              displayCrypto(cryptoData)
+              return cryptoData
+            })
+  
+        }
+      })
+  }
