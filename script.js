82ddfb1e27899a81bce87aeba867be364853360f
Ivan Zabrodin <pyrat32@gmail.com>
2021-02-06 10:01:28 -0600

    lazy commit message

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-let newsApiKey = 'feef1da8cb7732bae46deb391e9d0f83' // lol
-
-getNews('hi').then(data => console.log(data))
-
-function newsSearchHandler(event) {
+function stockSearchHandler(event) {
     let query = document.getElementById('newsSearch').nodeValue
     getNews(query).then(data => {
-
+        
     })
 }
 
-fetch('http://api.datanews.io/v1/news?q=SpaceX&from=2020-07-01&to=2020-09-10&language=en&apiKey=0aszfc72ctgxr4e947mg1sr67', {
-    method: 'get',
-    headers: {
-        'Access-Control-Allow-Origin': '*'    
-    }
+fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=U65M3D2LOCIOUFEM', {
 }).then(response => response.json()).then(data =>console.log(data))
